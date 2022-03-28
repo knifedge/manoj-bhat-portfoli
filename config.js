f3d9d34e1362d3871d0140c64515d7765fbf3df4
knifedge <imanojbhat@gmail.com>
2022-03-28 13:28:48 +0530

    feat: about me and link updates

--- config.js
+++ config.js
@@ -1,50 +1,46 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
-  postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
-  useKatex: false,
-  menu: [
-    {
-      label: 'Articles',
-      path: '/'
-    },
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
-  ],
-  author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-    contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-      medium: '',
-    }
-  }
+	url: "https://lumen.netlify.com",
+	pathPrefix: "/",
+	title: "My Blog",
+	subtitle: "Product Engineer @ DIY.org",
+	copyright: "© All rights reserved.",
+	disqusShortname: "",
+	postsPerPage: 4,
+	googleAnalyticsId: "UA-73379983-2",
+	useKatex: false,
+	menu: [
+		{
+			label: "Articles",
+			path: "/",
+		},
+		{
+			label: "About me",
+			path: "/pages/about",
+		},
+	],
+	author: {
+		name: "Manoj Bhat",
+		photo: "/photo.jpg",
+		bio: "Product Engineer @ DIY.org",
+		contacts: {
+			email: "imanojbhat@gmail.com",
+			facebook: "",
+			telegram: "t.me/mbhat94",
+			twitter: "https://twitter.com/ManojBhat38",
+			github: "https://github.com/knifedge",
+			rss: "",
+			vkontakte: "",
+			linkedin: "https://www.linkedin.com/in/manoj-bhat/",
+			instagram: "https://www.instagram.com/manoj_____bhat/",
+			line: "",
+			gitlab: "",
+			weibo: "",
+			codepen: "",
+			youtube: "",
+			soundcloud: "",
+			medium: "",
+		},
+	},
 };
